fd989e8e4bf333a5e804bc675ee7214ff02b9782
Ssabae <llsb156@gmail.com>
2020-05-31 06:01:32 +0900

    Session User 객체 Annotation으로 가져오기

---
+++ src/main/java/kr/co/ssabae/aws/springboot/config/auth/LoginUser.java
@@ -0,0 +1,12 @@
+package kr.co.ssabae.aws.springboot.config.auth;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface LoginUser {
+
+}
--- src/main/java/kr/co/ssabae/aws/springboot/web/IndexController.java
+++ src/main/java/kr/co/ssabae/aws/springboot/web/IndexController.java
@@ -1,5 +1,6 @@
 package kr.co.ssabae.aws.springboot.web;
 
+import kr.co.ssabae.aws.springboot.config.auth.LoginUser;
 import kr.co.ssabae.aws.springboot.config.auth.dto.SessionUser;
 import kr.co.ssabae.aws.springboot.service.posts.PostsService;
 import kr.co.ssabae.aws.springboot.web.dto.PostsResponseDto;
@@ -20,9 +21,8 @@ public class IndexController {
     private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
-        SessionUser user = (SessionUser) httpSession.getAttribute("user");
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
