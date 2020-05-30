59a70d01a023c7d03a880418926e04c65cb9e6b1
Ssabae <llsb156@gmail.com>
2020-05-31 05:49:15 +0900

    Goggle 로그인 연동

---
+++ src/main/java/kr/co/ssabae/aws/springboot/config/auth/SecurityConfig.java
@@ -0,0 +1,34 @@
+package kr.co.ssabae.aws.springboot.config.auth;
+
+import kr.co.ssabae.aws.springboot.domain.user.Role;
+import kr.co.ssabae.aws.springboot.service.CustomOAuth2UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@RequiredArgsConstructor
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomOAuth2UserService customOAuth2UserService;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                    .authorizeRequests()
+                    .antMatchers("/", "/css/**", "/images/**", "/js/**", "/h2-console").permitAll()
+                    .antMatchers("/api/v1/**").hasRole(Role.USER.name())
+                    .anyRequest().authenticated()
+                .and()
+                    .logout()
+                        .logoutSuccessUrl("/")
+                .and()
+                    .oauth2Login()
+                        .userInfoEndpoint()
+                            .userService(customOAuth2UserService);
+    }
+}
